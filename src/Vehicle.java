74d0b0338b7f49140d2c5f1e2f7b593a65e5c362
Shamar Bennett <114320976+ShamarTbennett@users.noreply.github.com>
2026-01-04 05:30:04 -0800

    Added the vehicle class

---
+++ src/Vehicle.java
@@ -0,0 +1,24 @@
+public class Vehicle{
+
+    protected String licenseNum;
+    protected String brand;
+    protected String model;
+    protected int year;
+    protected String color;
+    protected double engineSize;
+    protected String fueltype;
+    protected Strings transmissiontype;
+    protected int mileage;
+    protected int seatCapacity;
+    protected String resntalStatus;
+    protected int ratesPerday;
+
+    //default constructor
+    public Vehicle(){
+        this.licenseNum = "ABCDEF";
+        this.brand = "New";
+        this.model = "Box"
+        
+    }
+
+}
