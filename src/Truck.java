ff89ff2ce9b3729445278936b9df41a37f50d72c
Shamar Bennett <bennettshamar96@gmail.com>
2026-01-05 19:37:20 -0500

    added code to truck class

--- src/Truck.java
+++ src/Truck.java
@@ -1,3 +1,10 @@
 public class Truck extends Vehicle{
     
+    private double towingcapacity;
+
+    // Default Constructor
+    public Truck(){
+        super();
+        this.towingcapacity = 1000.0;
+    }
 }
