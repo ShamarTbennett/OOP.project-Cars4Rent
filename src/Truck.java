e1721b1069611fdd4eb291861210baf6d2069ea1
Shamar Bennett <bennettshamar96@gmail.com>
2026-01-06 21:44:13 -0500

    added code to the truck class

--- src/Truck.java
+++ src/Truck.java
@@ -7,4 +7,16 @@ public class Truck extends Vehicle{
         super();
         this.towingcapacity = 1000.0;
     }
+
+    // Primary (Parameterized) Constructor
+    public Truck(String licenseNum, String brand, String model, int year,
+                 String color, double engineSize, String fueltype,
+                 String transmissiontype, int mileage, int seatCapacity,
+                 String resntalStatus, int ratesPerday, double towingcapacity) {
+        super(licenseNum, brand, model, year, color, engineSize, fueltype,
+              transmissiontype, mileage, seatCapacity, resntalStatus, ratesPerday);
+        this.towingcapacity = towingcapacity;
+    }
+
+    
 }
