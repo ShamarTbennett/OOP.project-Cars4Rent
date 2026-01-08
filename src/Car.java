98d66ef6526d7b8115c4735d7f0765e5a7231d36
Shamar Bennett <bennettshamar96@gmail.com>
2026-01-08 17:05:00 -0500

    car file

--- src/Car.java
+++ src/Car.java
@@ -139,7 +139,7 @@ public class Car extends Vehicle{
         String vehicle = licenseNum + "\t\t\t" + type + "\t  \t  " + brand + "\t" +
                          model + "\t " + year + "\t " + color + "\t " + engineSize + "\t   \t\t  " +
                          fueltype + "\t\t " + transmissiontype + "\t\t\t  " + mileage + "\t\t\t  " +
-                         seatCapacity + "\t\t  " + ratesPerday + "\t" + interiortype + "\t" + resntalStatus + "\n";
+                         seatCapacity + "\t\t " + ratesPerday + "\t" + interiortype + "\t" + resntalStatus + "\n";
 
         instream.write(vehicle);
         instream.close();
