04f61cb90f5ae5052f7f771a9e2a6146071d3d3f
Shamar Bennett <bennettshamar96@gmail.com>
2026-01-15 10:19:58 -0500

    trying to fix the display all method in rental.java

--- src/Rental.java
+++ src/Rental.java
@@ -31,31 +31,31 @@ public void viewvehicles() {
 
     try (Scanner infileStream = new Scanner(new File("Vehicle.txt"))) {
 
-        while (infileStream.hasNextLine()) {
+        while (infileStream.hasNext()) {
 
-            this.licenseNum = infileStream.nextLine();
-            this.type = infileStream.nextLine();
-            this.brand = infileStream.nextLine();
-            this.model = infileStream.nextLine();
+            this.licenseNum = infileStream.next();
+            this.type = infileStream.next();
+            this.brand = infileStream.next();
+            this.model = infileStream.next();
 
-            this.year = Integer.parseInt(infileStream.nextLine());
-            this.color = infileStream.nextLine();
-            this.engineSize = Double.parseDouble(infileStream.nextLine());
+            this.year = infileStream.nextInt();
+            this.color = infileStream.next();
+            this.engineSize = infileStream.nextDouble();
 
-            this.fueltype = infileStream.nextLine();
-            this.transmissiontype = infileStream.nextLine();
+            this.fueltype = infileStream.next();
+            this.transmissiontype = infileStream.next();
 
-            this.mileage = Integer.parseInt(infileStream.nextLine());
-            this.seatCapacity = Integer.parseInt(infileStream.nextLine());
-            this.ratesPerday = Integer.parseInt(infileStream.nextLine());
+            this.mileage = infileStream.nextInt();
+            this.seatCapacity = infileStream.nextInt();
+            this.ratesPerday = infileStream.nextInt();
 
-            this.towingCapacity = Double.parseDouble(infileStream.nextLine());
-            this.numberOfHelmets = Integer.parseInt(infileStream.nextLine());
+            this.towingCapacity = infileStream.nextInt();
+            this.numberOfHelmets = infileStream.nextInt();
+            this.interiortype = infileStream.next();
 
-            this.interiortype = infileStream.nextLine();
-            this.rentalStatus = infileStream.nextLine();
+            this.rentalStatus = infileStream.next();
 
-            System.out.println(this);
+            System.out.println(toString());
         }
 
     } catch (FileNotFoundException e) {
