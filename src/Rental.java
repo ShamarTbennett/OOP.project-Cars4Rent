10de23d5516c68925ec59643738f6dd336f694f9
Shamar Bennett <114320976+ShamarTbennett@users.noreply.github.com>
2026-02-05 11:50:06 -0500

    f

--- src/Rental.java
+++ src/Rental.java
@@ -159,7 +159,7 @@ public class Rental extends Vehicle {
 
             if (match) {
                 System.out.println(v); // prints EACH matching vehicle
-                System.out.println("i am still on vacatdgdggffion");
+                System.out.println();
                 
                 
             }
