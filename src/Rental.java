1c33240a70859fddf80eb9c16ef6b4f779aab773
Shamar Bennett <bennettshamar96@gmail.com>
2026-02-18 17:49:12 -0500

    t

--- src/Rental.java
+++ src/Rental.java
@@ -158,7 +158,7 @@ public class Rental extends Vehicle {
                         String.valueOf(v.year).equals(search);
 
                 if (match) {
-                    display(); // prints EACH matching vehicl             
+                    Display(); // prints EACH matching vehicl             
                     
                     
                 }
@@ -170,7 +170,8 @@ public class Rental extends Vehicle {
     }
 
 
-    public void display() {
+    @Override
+    public void Display() {
 
         System.out.println("License Plate \tType\t Brand\t Model\t\tYear\tColor\tEngineSize(L)\tFuelType\tTransmission\tMileage(km)\tSeats\tRatePerDay($)\tStatus\tInterior\tHelmets\tTowingCap");
         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
